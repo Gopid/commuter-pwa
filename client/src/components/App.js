26a2494fa1fb36936b9085e9e40718d09fa62cd3
Gyure <akos.gyure@accenture.com>
2018-05-15 08:17:42 +0200

    heading for arrivals section

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,16 +7,17 @@ const App = () => (
             <div className="direction__container">
                 <h1 className="direction__title">Select Your Destination</h1>
                 <div className="direction__options">
-                    <img className="direction__icon" src="/img/home.svg" alt="" />
+                    <img className="direction__icon direction__icon--active" src="/img/home.svg" alt="" />
                     <img className="direction__icon" src="/img/office.svg" alt="" />
                 </div>
                 <ToggleButton
                     onContent={() => <span>Start sending notifications</span>}
-                    offContent={() => <span>Pause sending notifications</span>}
+                    offContent={() => <span>Stop sending notifications</span>}
                 />
             </div>
         </section>
         <section id="details">
+            <h1 className="details__title">Arrivals to your destination</h1>
             <article className="card">
                 <div className="circle"></div>
                 <p>20E</p>
